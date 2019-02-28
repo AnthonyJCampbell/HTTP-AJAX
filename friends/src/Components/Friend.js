b5cff47f63672fbfbc2655e1ad373aa72d360f76
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-28 14:07:51 +0100

    Added FontAwesome

--- friends/src/Components/Friend.js
+++ friends/src/Components/Friend.js
@@ -3,9 +3,14 @@ import React from 'react';
 const Friend = ({ friend, deleteUser }) => {
     return (
         <div>
-            <h2>{friend.name} ({friend.age})</h2>
+            <div className="friendTop"
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"}}>
+                <h2>{friend.name} ({friend.age}) </h2><i onClick={() => deleteUser(friend.id)}     className="fa fa-window-close" aria-hidden="true"></i>
+            </div>
             <p><a href="/">{friend.email}</a></p>
-            <button onClick={() => deleteUser(friend.id)}>Delete</button>
         </div>
     );
 }
